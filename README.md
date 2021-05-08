7139f4ef333b0a9ee757787f1449b03d85055a62
Yuunan Kin <yuunan.kin@gmail.com>
2021-05-08 11:34:48 +0900

    add electron

--- README.md
+++ README.md
@@ -5,3 +5,12 @@
 - react-scripts(instead of webpack5)
 - firebase auth
 - firebase realtime database
+
+#### Problems
+
+- tsconfig.json: `"noEmit": true` is needed by react-scripts but cause problem in electron
+
+#### build
+
+- react: `npm run build`
+- electron: comment out `"noEmit": true` and `npm run dev:electron`
---
+++ electron/main.ts
@@ -0,0 +1,88 @@
+import { app, BrowserWindow, Menu, Notification } from "electron";
+import installExtension, {
+  REACT_DEVELOPER_TOOLS,
+  REDUX_DEVTOOLS,
+} from "electron-devtools-installer";
+import isDev from "electron-is-dev";
+import { ipcMain } from "electron";
+import axios from "axios";
+import * as url from "url";
+import path from "path";
+
+const isMac = process.platform === "darwin" ? true : false;
+let mainWindow: BrowserWindow | null;
+const createMainWindow = (): void => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+    },
+  });
+  mainWindow.loadURL(
+    // isDev
+    //   ? "http://localhost:4000"
+    //   :
+    url.format({
+      pathname: path.join(__dirname, "index.html"),
+      protocol: "file:",
+      slashes: true,
+    })
+  );
+  if (isDev) {
+    //install devtools
+    installExtension(REACT_DEVELOPER_TOOLS)
+      .then((name) => console.log(`Added Extension:  ${name}`))
+      .catch((err) => console.log("An error occurred: ", err));
+    installExtension(REDUX_DEVTOOLS)
+      .then((name) => console.log(`Added Extension:  ${name}`))
+      .catch((err) => console.log("An error occurred: ", err));
+  }
+  // menu
+  const mainMenu = Menu.buildFromTemplate(menu as any);
+  Menu.setApplicationMenu(mainMenu);
+  new Notification({
+    title: "Welcome",
+    body: "App started.",
+  }).show();
+  mainWindow.on("closed", () => (mainWindow = null));
+};
+const menu = [
+  ...(isMac ? [{ role: "appMenu" }] : []),
+  {
+    role: "fileMenu",
+  },
+  ...(isDev
+    ? [
+        {
+          label: "Developer",
+          submenu: [
+            { role: "reload" },
+            { role: "forcereload" },
+            { type: "separator" },
+            { role: "toggledevtools" },
+          ],
+        },
+      ]
+    : []),
+];
+
+app.on("ready", createMainWindow);
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createMainWindow();
+  }
+});
+
+// Stop error
+app.allowRendererProcessReuse = true;
+
+// events
